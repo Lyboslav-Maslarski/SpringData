683badee2d2ccab5ce949972a29547ecc9a74811
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-05-04 14:27:36 +0300

    add

---
+++ SpringDataAdvancedQuering - Exercise/src/main/java/example/model/entity/EditionType.java
@@ -0,0 +1,5 @@
+package example.model.entity;
+
+public enum EditionType {
+    NORMAL, PROMO, GOLD
+}
---
+++ SpringDataAutoMappingObjects - Lab/src/main/java/example/entities/Employee.java
@@ -0,0 +1,104 @@
+package example.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private BigDecimal salary;
+    private LocalDate birthday;
+    @ManyToOne
+    private Address address;
+    private Boolean isOnHoliday;
+    @ManyToOne
+    private Employee manager;
+    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Employee> employees;
+
+    public Employee() {
+        this.employees = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Boolean getOnHoliday() {
+        return isOnHoliday;
+    }
+
+    public void setOnHoliday(Boolean onHoliday) {
+        isOnHoliday = onHoliday;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
