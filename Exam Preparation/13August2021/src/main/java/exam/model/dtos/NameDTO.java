243531469a797ee2de0e25cbd2b1ec884a49a63a
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-05-08 00:18:37 +0300

    add

---
+++ Exam Preparation/13August2021/src/main/java/exam/model/dtos/NameDTO.java
@@ -0,0 +1,17 @@
+package exam.model.dtos;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class NameDTO {
+    @XmlElement
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
