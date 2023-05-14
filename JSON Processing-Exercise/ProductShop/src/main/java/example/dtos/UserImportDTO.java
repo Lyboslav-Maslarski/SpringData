2a416e4519023e0da4b67b6bb20a73de25dc4d1f
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-05-15 00:25:31 +0300

    add

---
+++ JSON Processing-Exercise/ProductShop/src/main/java/example/dtos/UserImportDTO.java
@@ -0,0 +1,19 @@
+package example.dtos;
+
+public class UserImportDTO {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
